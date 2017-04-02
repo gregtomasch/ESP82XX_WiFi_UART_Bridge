6496d40e56484170e7274eb9ba2312cf5841fb78
Greg Tomasch <poirot@earthlink.net>
2017-04-02 00:28:01 -0700

    Add files via upload

---
+++ Host_MCU_Integration_Example/WiFiserial.cpp
@@ -0,0 +1,521 @@
+#include "Arduino.h"
+#include "WiFiSerial.h"
+
+WiFiSerial::WiFiSerial()
+{
+}
+
+/**
+* @fn: serialCom()
+*
+* @brief: Main MultiWii Serial Protocol (MSP) function; manages port state and packet logistics
+* @params:
+* @returns:
+*/
+void WiFiSerial::serialCom()
+{
+  uint8_t c,n;  
+
+  for(n=1; n < UART_NUMBER; n++)                                                                                          // General function to handle multiple active ports. Limited to UART1 - UART3 for the IU Butterfly-based board
+  {
+    CURRENTPORT = n;
+    while (WiFiSerial::SerialAvailable(CURRENTPORT))
+    {
+      // Indicates the number of occupied bytes in TX buffer
+      uint8_t bytesTXBuff = ((uint8_t)(serialHeadTX[CURRENTPORT]-serialTailTX[CURRENTPORT]))%TX_BUFFER_SIZE;
+
+      // Ensure there is enough free TX buffer to go further (50 bytes margin)
+      if (bytesTXBuff > TX_BUFFER_SIZE - 50 ) return;
+      c = WiFiSerial::SerialRead(CURRENTPORT);
+      if (c_state[CURRENTPORT] == IDLE)
+      {
+        c_state[CURRENTPORT] = (c=='$') ? HEADER_START : IDLE;
+      }else if (c_state[CURRENTPORT] == HEADER_START)
+      {
+        c_state[CURRENTPORT] = (c=='M') ? HEADER_M : IDLE;
+      } else if (c_state[CURRENTPORT] == HEADER_M)
+      {
+        c_state[CURRENTPORT] = (c=='<') ? HEADER_ARROW : IDLE;
+      } else if (c_state[CURRENTPORT] == HEADER_ARROW)
+      { 
+        // Now we are expecting the payload size
+        if (c > INBUF_SIZE)
+        {
+          c_state[CURRENTPORT] = IDLE;
+          continue;
+        }
+        dataSize[CURRENTPORT] = c;
+        offset[CURRENTPORT] = 0;
+        checksum[CURRENTPORT] = 0;
+        indRX[CURRENTPORT] = 0;
+        checksum[CURRENTPORT] ^= c;
+
+        // The command is to follow
+        c_state[CURRENTPORT] = HEADER_SIZE;
+      } else if (c_state[CURRENTPORT] == HEADER_SIZE)
+      {
+        cmdWiFiSerial[CURRENTPORT] = c;
+        checksum[CURRENTPORT] ^= c;
+        c_state[CURRENTPORT] = HEADER_CMD;
+      } else if (c_state[CURRENTPORT] == HEADER_CMD && offset[CURRENTPORT] < dataSize[CURRENTPORT])
+      {
+        checksum[CURRENTPORT] ^= c;
+        inBuf[offset[CURRENTPORT]++][CURRENTPORT] = c;
+      } else if (c_state[CURRENTPORT] == HEADER_CMD && offset[CURRENTPORT] >= dataSize[CURRENTPORT])
+      {
+        // Compare calculated and transferred checksum
+        if (checksum[CURRENTPORT] == c) 
+        {
+          // We got a valid packet, evaluate it
+          WiFiSerial::evaluateCommand();
+        }
+        c_state[CURRENTPORT] = IDLE;
+      }
+    }
+  }
+}
+
+/**
+* @fn: evaluateCommand()
+*
+* @brief: Main (MSP) command decoder
+* @params:
+* @returns:
+*/
+void WiFiSerial::evaluateCommand()
+{
+  switch(cmdWiFiSerial[CURRENTPORT])
+  {
+   case RECEIVE_SERIAL_DATA:                                                                                              // CMD 10
+     for(uint8_t i=0; i < (UPDATE_SIZE-1); i++)
+     {
+       data_update[i] = WiFiSerial::readfloat();
+     }
+     break;
+   case SEND_SERIAL_DATA:                                                                                                 // CMD 20
+     cmdWiFiSerial[BUTTERFLY_WIFI_UART] = 10;
+     WiFiSerial::headSerialRequest(BUTTERFLY_WIFI_UART, 0, 4*(UPDATE_SIZE));
+     for(uint8_t i=0; i < (UPDATE_SIZE-1); i++)
+     {
+       WiFiSerial::serializefloat(data_update[i]);
+     }
+     WiFiSerial::tailSerialRequest(BUTTERFLY_WIFI_UART);
+   default:
+     break;
+  }
+}
+
+/**
+* @fn: SerialSendMWPcmd(uint8_t port, uint8_t err,uint8_t cmd)
+*
+* @brief: Sends a data request command to the remote host
+* @params: port, command
+* @returns:
+*/
+void WiFiSerial::SerialSendMWPcmd(uint8_t port, uint8_t err,uint8_t cmd)
+{
+  // Buffer previous command value for this port
+  uint8_t cmd_prev = cmdWiFiSerial[port];
+  
+  cmdWiFiSerial[port] = cmd;
+  WiFiSerial::headSerialRequest(port, err, 0);
+  WiFiSerial::tailSerialRequest(port);
+
+  // Restore previous command value for this port
+  cmdWiFiSerial[port] = cmd_prev;
+}
+
+/**
+* @fn:  WiFiSerial::Push_Data_Update()
+*
+* @brief: Pushes the current values of output variables into an update message; data variables treated as SP floats
+* @params:
+* @returns:
+*/
+void WiFiSerial::Push_Data_Update()
+{
+  CURRENTPORT = BUTTERFLY_WIFI_UART;
+  cmdWiFiSerial[BUTTERFLY_WIFI_UART] = 10;
+  WiFiSerial::headSerialRequest(BUTTERFLY_WIFI_UART, 0, 4*(UPDATE_SIZE));
+  for(uint8_t i=0; i < (UPDATE_SIZE); i++)
+  {
+    WiFiSerial::serializefloat(data_update[i]);
+  }
+  WiFiSerial::tailSerialRequest(BUTTERFLY_WIFI_UART);
+}
+
+/**
+* @fn: readfloat()
+*
+* @brief: Read 4bytes from the input buffer and converts to float
+* @params:
+* @returns:
+*/
+float WiFiSerial::readfloat()
+{
+  union
+  {
+    uint8_t float_data_byte[4];
+    float float_data;
+  } f;
+  
+  for( uint8_t i=0; i<4; i++)
+  {
+    f.float_data_byte[i] = WiFiSerial::read8();
+  }
+  return f.float_data;
+}
+
+/**
+* @fn: read32()
+*
+* @brief: Read 4bytes from the input buffer
+* @params:
+* @returns:
+*/
+uint32_t WiFiSerial::read32()
+{
+  uint32_t t = WiFiSerial::read16();
+  t+= (uint32_t)WiFiSerial::read16()<<16;
+  return t;
+}
+
+/**
+* @fn: read16()
+*
+* @brief: Read 2bytes from the input buffer
+* @params:
+* @returns:
+*/
+uint16_t WiFiSerial::read16()
+{
+  uint16_t t = WiFiSerial::read8();
+  t+= (uint16_t)WiFiSerial::read8()<<8;
+  return t;
+}
+
+/**
+* @fn: read8()
+*
+* @brief: Read 1byte from the input buffer
+* @params:
+* @returns:
+*/
+uint8_t WiFiSerial::read8()
+{
+  return inBuf[indRX[CURRENTPORT]++][CURRENTPORT]&0xff;
+}
+
+/**
+* @fn: headSerialRequest(uint8_t port, uint8_t err, uint8_t s)
+*
+* @brief: Construct serial request header
+* @params: Port, error state and message size in bytes
+* @returns:
+*/
+void WiFiSerial::headSerialRequest(uint8_t port, uint8_t err, uint8_t s)
+{
+  WiFiSerial::serialize8('$');
+  WiFiSerial::serialize8('M');
+  WiFiSerial::serialize8(err ? '!' : '<');
+  
+  // start calculating a new checksum
+  checksum[port] = 0;
+  WiFiSerial::serialize8(s);
+  WiFiSerial::serialize8(cmdWiFiSerial[port]);
+}
+
+/**
+* @fn: headSerialResponse(uint8_t err, uint8_t s)
+*
+* @brief: Construct serial reply header
+* @params: Error state and reply size in bytes
+* @returns:
+*/
+void WiFiSerial::headSerialResponse(uint8_t err, uint8_t s)
+{
+  WiFiSerial::serialize8('$');
+  WiFiSerial::serialize8('M');
+  WiFiSerial::serialize8(err ? '!' : '>');
+  
+  // start calculating a new checksum
+  checksum[CURRENTPORT] = 0;
+  WiFiSerial::serialize8(s);
+  WiFiSerial::serialize8(cmdWiFiSerial[CURRENTPORT]);
+}
+
+/**
+* @fn: headSerialReply(uint8_t s)
+*
+* @brief: Construct serial reply header with error state "0"
+* @params: Reply size in bytes
+* @returns:
+*/
+void WiFiSerial::headSerialReply(uint8_t s)
+{
+  WiFiSerial::headSerialResponse(0, s);
+}
+
+/**
+* @fn: headSerialError(uint8_t s)
+*
+* @brief: Construct serial reply header with error state "1"
+* @params: Reply size in bytes
+* @returns:
+*/
+void inline WiFiSerial::headSerialError(uint8_t s)
+{
+  WiFiSerial::headSerialResponse(1, s);
+}
+
+/**
+* @fn: tailSerialRequest()
+*
+* @brief: Construct and transmit checksum data for end of packet
+* @params:
+* @returns:
+*/
+void WiFiSerial::tailSerialRequest(uint8_t port)
+{
+  WiFiSerial::serialize8(checksum[port]);
+  WiFiSerial::UartSendData(port);
+}
+
+/**
+* @fn: tailSerialReply()
+*
+* @brief: Construct and transmit checksum data for end of packet
+* @params:
+* @returns:
+*/
+void WiFiSerial::tailSerialReply()
+{
+  WiFiSerial::serialize8(checksum[CURRENTPORT]);
+  WiFiSerial::UartSendData(CURRENTPORT);
+}
+
+/**
+* @fn: serializeNames(PGM_P s)
+*
+* @brief: "Serialize" ASCII into bytes for serial transmission
+* @params:
+* @returns:
+*/
+void WiFiSerial::serializeNames(PGM_P s)
+{
+  for (PGM_P c = s; pgm_read_byte(c); c++)
+  {
+    WiFiSerial::serialize8(pgm_read_byte(c));
+  }
+}
+
+/**
+* @fn: serializefloat(float a)
+*
+* @brief: "Serialize" a float into four bytes for serial transmission
+* @params:
+* @returns:
+*/
+void WiFiSerial::serializefloat(float a)
+{
+  union
+  {
+    float float_data;
+    uint8_t float_data_byte[4];
+  } f;
+  f.float_data = a;
+  for( uint8_t i=0; i<4; i++)
+  {
+    WiFiSerial::serialize8(f.float_data_byte[i]);
+  }
+}
+
+/**
+* @fn: serialize32(uint32_t a)
+*
+* @brief: "Serialize" a 32bit unsigned integer into four bytes for serial transmission
+* @params:
+* @returns:
+*/
+void WiFiSerial::serialize32(uint32_t a)
+{
+  WiFiSerial::serialize8((a    ) & 0xFF);
+  WiFiSerial::serialize8((a>> 8) & 0xFF);
+  WiFiSerial::serialize8((a>>16) & 0xFF);
+  WiFiSerial::serialize8((a>>24) & 0xFF);
+}
+
+/**
+* @fn: serialize16(int16_t a)
+*
+* @brief: "Serialize" a 16bit signed integer into four bytes for serial transmission
+* @params:
+* @returns:
+*/
+void WiFiSerial::serialize16(int16_t a)
+{
+  WiFiSerial::serialize8((a   ) & 0xFF);
+  WiFiSerial::serialize8((a>>8) & 0xFF);
+}
+
+/**
+* @fn: serialize16(uint8_t a)
+*
+* @brief: "Serialize" an 8bit unsigned integer into four bytes for serial transmission
+* @params:
+* @returns:
+*/
+void WiFiSerial::serialize8(uint8_t a)
+{
+  uint8_t t = serialHeadTX[CURRENTPORT];
+  if (++t >= TX_BUFFER_SIZE) t = 0;
+  serialBufferTX[t][CURRENTPORT] = a;
+  checksum[CURRENTPORT] ^= a;
+  serialHeadTX[CURRENTPORT] = t;
+}
+
+/**
+* @fn: T_USB_Available(uint8_t port)
+*
+* @brief: Returns the number of bytes available to be read from port
+* @params:
+* @returns:
+*/
+unsigned char WiFiSerial::T_USB_Available(uint8_t port)
+{
+  int n ; 
+  switch (port)
+  {
+    case 0: n= Serial.available(); break;
+    case 1: n= Serial1.available(); break;
+    case 2: n= Serial2.available(); break;
+    case 3: n= Serial3.available(); break;
+    default: n=0; break;
+  }
+  if (n > 255) n = 255;
+  return n;
+}
+
+/**
+* @fn: T_USB_Write(uint8_t port, uint8_t uc_data)
+*
+* @brief: Writes specified data to specified port
+* @params: Data to be written, port to be written to
+* @returns: Number of btes written
+*/
+unsigned char WiFiSerial::T_USB_Write(uint8_t port, uint8_t uc_data)
+{
+  int n ; 
+  switch (port)
+  {
+    case 0: n= Serial.write(uc_data); break;
+    case 1: n= Serial1.write(uc_data); break;
+    case 2: n= Serial2.write(uc_data); break;
+    case 3: n= Serial3.write(uc_data); break;
+    default: n=0; break;
+  }
+  return n;
+}
+
+
+/**
+* @fn: UartSendData(uint8_t port)
+*
+* @brief: Writes TX buffer to serial port
+* @params: Port to be written to
+* @returns:
+*/
+void WiFiSerial::UartSendData(uint8_t port)
+{
+  while(serialHeadTX[port] != serialTailTX[port])
+  {
+    if (++serialTailTX[port] >= TX_BUFFER_SIZE) serialTailTX[port] = 0;
+    WiFiSerial::T_USB_Write (port,serialBufferTX[serialTailTX[port]][port]);
+  }
+}
+
+/**
+* @fn: SerialTXfree(uint8_t port)
+*
+* @brief: Determines if serial port is free (TX buffer empty)
+* @params: Port to be written to
+* @returns: Boolean regarding port availability
+*/
+bool WiFiSerial::SerialTXfree(uint8_t port)
+{
+  return (serialHeadTX[port] == serialTailTX[port]);
+}
+
+/**
+* @fn: SerialOpen(uint8_t port, uint32_t baud)
+*
+* @brief: Opens serial port for use
+* @params: Port to be initialized, baud rate
+* @returns:
+*/
+void WiFiSerial::SerialOpen(uint8_t port, uint32_t baud)
+{
+  switch (port)
+  {
+    case 0: Serial.begin(baud);  break;
+    case 1: Serial1.begin(baud); break;
+    case 2: Serial2.begin(baud); break;
+    case 3: Serial3.begin(baud); break;
+  }
+}
+
+/**
+* @fn: SerialEnd(uint8_t port)
+*
+* @brief: Closes serial port
+* @params: Port to be closed
+* @returns:
+*/
+void WiFiSerial::SerialEnd(uint8_t port)
+{
+  switch (port)
+  {
+      case 0: Serial.end(); break;
+      case 1: Serial1.end(); break;
+      case 2: Serial2.end(); break;
+      case 3: Serial3.end(); break;
+  }
+}
+
+/**
+* @fn: SerialRead(uint8_t port)
+*
+* @brief: Reads a byte from a serialport
+* @params: Port to be read
+* @returns:
+*/
+uint8_t WiFiSerial::SerialRead(uint8_t port)
+{
+  switch (port)
+  {
+    case 0: return Serial.read();
+    case 1: return Serial1.read();
+    case 2: return Serial2.read();
+    case 3: return Serial3.read();
+  }
+  return 0;
+}
+
+uint8_t WiFiSerial::SerialAvailable(uint8_t port)
+{
+  return WiFiSerial::T_USB_Available(port);
+}
+
+void WiFiSerial::SerialSerialize(uint8_t port,uint8_t a)
+{
+  uint8_t t = serialHeadTX[port];
+  if (++t >= TX_BUFFER_SIZE) t = 0;
+  serialBufferTX[t][port] = a;
+  serialHeadTX[port] = t;
+}
+
+void WiFiSerial::SerialWrite(uint8_t port,uint8_t c)
+{
+  CURRENTPORT=port;
+  SerialSerialize(port,c);WiFiSerial::UartSendData(port);
+}
---
+++ Host_MCU_Integration_Example/WiFiserial.h
@@ -0,0 +1,71 @@
+#ifndef WiFiSerial_h
+#define WiFiSerial_h
+
+// Configuration definitions
+#define UART_NUMBER                    4                                                              // Total number of possible serial ports on the STM32
+#define TX_BUFFER_SIZE                 512                                                            // 512 is a good size for general use, even GNSS devices
+#define INBUF_SIZE                     512                                                            // 512 is a good size for general use, even GNSS devices
+#define RECEIVE_SERIAL_DATA            10                                                             // Command 10, receive serial data from the ESP82XX
+#define SEND_SERIAL_DATA               20                                                             // Command 20, send serial data to the ESP82XX
+#define UPDATE_SIZE                    20                                                             // Size (in float data points) of the data update message pushed to the ESP82XX
+#define BUTTERFLY_WIFI_UART            3                                                              // Butterfly UART port connected to the ESP82XX to constitute the UART/WiFi bridge
+
+
+static enum _WiFiSerial_state
+                                      {
+                                        IDLE,
+                                        HEADER_START,
+                                        HEADER_M,
+                                        HEADER_ARROW,
+                                        HEADER_SIZE,
+                                        HEADER_CMD,
+                                      } c_state[UART_NUMBER];
+static volatile uint8_t                 serialHeadTX[UART_NUMBER],serialTailTX[UART_NUMBER];
+static uint8_t                          serialBufferTX[TX_BUFFER_SIZE][UART_NUMBER];
+static uint8_t                          inBuf[INBUF_SIZE][UART_NUMBER];
+static uint8_t                          checksum[UART_NUMBER];
+static uint8_t                          indRX[UART_NUMBER];
+static uint8_t                          cmdWiFiSerial[UART_NUMBER];
+static uint8_t                          CURRENTPORT = 0;
+static uint8_t                          offset[UART_NUMBER];
+static uint8_t                          dataSize[UART_NUMBER];
+
+extern float                            data_update[UPDATE_SIZE];                                     // 1D array for data points to be sent over the UART bridge. DATA TREATED AS SP FLOATS
+
+class WiFiSerial
+{
+  public:
+                                       WiFiSerial();
+	   static void                       serialCom();
+	   static bool                       SerialTXfree(uint8_t port);
+	   static void                       SerialOpen(uint8_t port, uint32_t baud);
+	   static void                       SerialEnd(uint8_t port);
+	   static uint8_t                    SerialRead(uint8_t port);
+	   static uint8_t                    SerialAvailable(uint8_t port);
+	   static void                       SerialWrite(uint8_t port,uint8_t c);
+     static void                       SerialSendMWPcmd(uint8_t port, uint8_t err,uint8_t cmd);
+     static void                       Push_Data_Update();
+  private:
+     static float                      readfloat();
+     static uint32_t                   read32();
+     static uint16_t                   read16();
+     static uint8_t                    read8();
+     static void                       headSerialRequest(uint8_t port, uint8_t err, uint8_t s);
+     static void                       headSerialResponse(uint8_t err, uint8_t s);
+     static void                       headSerialReply(uint8_t s);
+     static void inline                headSerialError(uint8_t s);
+     static void                       evaluateCommand();
+     static void                       serializefloat(float a);
+	   static void                       serialize32(uint32_t a);
+	   static void                       serialize16(int16_t a);
+	   static void                       serialize8(uint8_t a);
+     static void                       serializeNames(PGM_P s);
+	   static unsigned char              T_USB_Available(uint8_t port);
+	   static unsigned char              T_USB_Write(uint8_t port, uint8_t uc_data);
+	   static void                       UartSendData(uint8_t port);
+	   static void                       SerialSerialize(uint8_t port,uint8_t a);
+     static void                       tailSerialRequest(uint8_t port);
+     static void                       tailSerialReply();
+};
+
+#endif // WiFiSerial_h
